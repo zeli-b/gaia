691bc5e1555f469a0f789da0ee6c9a09eae6cbe0
Sch <junhg0211@gmail.com>
2025-04-05 10:36:23 +0000

    default index

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+import { Quadtree } from "./quadtree.js";
+import { Area, Layer, Structure, Project } from "./project.js";
+
+window.Quadtree = Quadtree;
+window.Area = Area;
+window.Layer = Layer;
+window.Structure = Structure;
+
 let canvas;
+window.project = new Project("Untitled", new Layer("Layer 1"));
 
 document.addEventListener("DOMContentLoaded", () => {
   canvas = document.querySelector("#canvas");
