bd45eedd702ce1c17979962fc08d06d42435cd98
Sch <junhg0211@gmail.com>
2025-04-05 09:49:59 +0000

    add canvas

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ let canvas;
 
 document.addEventListener("DOMContentLoaded", () => {
   canvas = document.querySelector("#canvas");
+
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+  canvas.addEventListener("resize", e => {
+    e.target.width = e.target.clientWidth;
+    e.target.height = e.target.clientHeught;
+  });
 });
