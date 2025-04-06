0954779d3f8be59422ab0096cfa2e304ed441948
Sch <junhg0211@gmail.com>
2025-04-06 11:43:55 +0000

    fill background color

--- index.js
+++ index.js
@@ -7,15 +7,50 @@ window.Layer = Layer;
 window.Structure = Structure;
 
 let canvas;
+let ctx;
 window.project = new Project("Untitled", new Layer("Layer 1"));
 
 document.addEventListener("DOMContentLoaded", () => {
+  // canvas 불러오기 및 이벤트 추가
   canvas = document.querySelector("#canvas");
+  ctx = canvas.getContext('2d');
 
   canvas.width = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
   canvas.addEventListener("resize", e => {
     e.target.width = e.target.clientWidth;
     e.target.height = e.target.clientHeught;
+
+    processFrame();
   });
+
+  // 한프레임 처리
+  processFrame();
 });
+
+/**
+ * 계산 등 화면 렌더링에 필요한 부수적인 것들을
+ * 계산하는 함수. 프레임마다 혹은 이벤트 발생 시마다 실행됨
+ */
+function tick() {
+}
+
+/**
+ * 화면에 보이는 것들을 렌더링하는 함수
+ * 프레임마다 혹은 이벤트 발생 시마다 실행됨
+ */
+function render() {
+  // 배경 색 칠
+  ctx.fillStyle = "#5C94B3";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+/**
+ * tick과 render 함수를 한번에 실행하는 함수
+ *
+ * 이벤트 처리 시에는 이 함수를 콜해서 화면을 업데이트해주어야 한다.
+ */
+function processFrame() {
+  tick();
+  render();
+}
