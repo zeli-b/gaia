114dfc843510e58f624316c0801d8720419107d2
Photon616 <yisollove@gmail.com>
2025-04-08 20:27:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -196,3 +196,11 @@ window.addEventListener("touchend", e => {
     pinchCenter = null;
   }
 });
+
+// 스크롤 확대 축 기능
+window.addEventListener("wheel", e => {
+  window.camera.setXZoom(window.camera.xZoom * Math.exp(e.deltaY * 0.02));
+  window.camera.setYZoom(window.camera.yZoom * Math.exp(e.deltaY * 0.02));
+
+  processFrame();
+})
