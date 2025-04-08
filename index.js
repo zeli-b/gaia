88dcc52f3930c7eeeeb176773ea4f8bcd160f3cf
Sch <junhg0211@gmail.com>
2025-04-08 00:03:56 +0000

    fullscreen feature

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ window.Area = Area;
 window.Layer = Layer;
 window.Structure = Structure;
 
+/**
+ * 화면 크기 변경에 대응하여 캔버스의 크기를 재조정
+ */
+function resizeCanvas() {
+    canvas.width = 0;
+    canvas.height = 0;
+    canvas.width = canvas.clientWidth * window.devicePixelRatio;
+    canvas.height = canvas.clientHeight * window.devicePixelRatio;
+
+    processFrame();
+}
+
 let canvas;
 let ctx;
 window.project = new Project("Untitled", new Layer("Layer 1"));
@@ -15,20 +27,19 @@ window.camera = new Camera(0.5, 0.5, 1000.0);
 let projectStructureDiv;
 
 document.addEventListener("DOMContentLoaded", () => {
+  // 파츠 불러오기
+  const propertiesDiv = document.querySelector("#properties");
+  const toolbarDiv = document.querySelector("#toolbar");
+  const topbarDiv = document.querySelector("#topbar");
+  const bottombarDiv = document.querySelector("#bottombar");
+
   // canvas 불러오기 및 이벤트 추가
   canvas = document.querySelector("#canvas");
   ctx = canvas.getContext('2d');
 
   canvas.width = canvas.clientWidth * window.devicePixelRatio;
   canvas.height = canvas.clientHeight * window.devicePixelRatio;
-  window.addEventListener("resize", e => {
-    canvas.width = 0;
-    canvas.height = 0;
-    canvas.width = canvas.clientWidth * window.devicePixelRatio;
-    canvas.height = canvas.clientHeight * window.devicePixelRatio;
-
-    processFrame();
-  });
+  window.addEventListener("resize", resizeCanvas);
 
   // projectStructureDiv
   projectStructureDiv = document.querySelector("#project-structure");
@@ -50,6 +61,16 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     };
     input.click();
+  };
+
+  // Fullscreen 반응
+  const fullscreenDiv = document.querySelector("#fullscreen");
+  fullscreenDiv.onclick = () => {
+    propertiesDiv.style.display = "none";
+    toolbarDiv.style.display = "none";
+    topbarDiv.style.display = "none";
+    bottombarDiv.style.display = "none";
+    resizeCanvas();
   };
 
   // 한프레임 처리
